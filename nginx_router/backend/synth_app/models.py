44cd16dcedff04ca2b92d9ac85d6e24e87c6891e
Bennett Dixon <benbendixon@gmail.com>
2019-08-21 08:32:06 -0700

    created first migration of author and book relationship

--- nginx_router/backend/synth_app/models.py
+++ nginx_router/backend/synth_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    """
+        Author class for graphql api test
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Book(models.Model):
+    """
+         Movie class for graphql api test
+    """
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
